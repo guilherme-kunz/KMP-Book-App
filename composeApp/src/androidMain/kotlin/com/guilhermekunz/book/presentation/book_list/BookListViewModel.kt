15be87111ad40e44291b0277375b0aa6c5a949f1
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2024-11-19 15:23:21 -0300

    feat: Book List ViewModel

---
+++ composeApp/src/androidMain/kotlin/com/guilhermekunz/book/presentation/book_list/BookListViewModel.kt
@@ -0,0 +1,30 @@
+package com.guilhermekunz.book.presentation.book_list
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class BookListViewModel: ViewModel() {
+
+    private val _state = MutableStateFlow(BookListState())
+    val state = _state.asStateFlow()
+
+    fun onAction(action: BookListAction) {
+        when (action) {
+            is BookListAction.OnSearchQueryChanged -> {
+                _state.update {
+                    it.copy(searchQuery = action.query)
+                }
+            }
+            is BookListAction.OnBookClick -> {
+
+            }
+            is BookListAction.OnTabSelected -> {
+                _state.update {
+                    it.copy(selectedTabIndex = action.index)
+                }
+            }
+        }
+    }
+}
