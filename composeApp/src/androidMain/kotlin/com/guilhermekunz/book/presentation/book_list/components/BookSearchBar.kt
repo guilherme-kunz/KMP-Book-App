e1ee892b88bde6ee1ccc116b67bbb53e2555e680
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2024-11-21 16:32:07 -0300

    feat: wip Book Search Bar

--- composeApp/src/androidMain/kotlin/com/guilhermekunz/book/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/androidMain/kotlin/com/guilhermekunz/book/presentation/book_list/components/BookSearchBar.kt
@@ -1,8 +1,24 @@
 package com.guilhermekunz.book.presentation.book_list.components
 
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.guilhermekunz.core.presentation.DarkBlue
+import com.guilhermekunz.core.presentation.SandYellow
+import kmp_book_app.composeapp.generated.resources.Res
+import kmp_book_app.composeapp.generated.resources.search_hint
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun BookSearchBar(
@@ -13,5 +29,46 @@ fun BookSearchBar(
 ) {
     OutlinedTextField(
         value = searchQuery,
-        onValueChange = onSearchQueryChanged)
+        onValueChange = onSearchQueryChanged,
+        shape = RoundedCornerShape(100),
+        colors = OutlinedTextFieldDefaults.colors(
+            cursorColor = DarkBlue,
+            focusedBorderColor = SandYellow
+        ),
+        placeholder = {
+            Text(
+                text = stringResource(Res.string.search_hint)
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+            )
+        },
+        singleLine = true,
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onImeSearch()
+            }
+        ),
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = searchQuery.isNotBlank()
+            ) {
+                IconButton(
+                    onClick = {
+                        onSearchQueryChanged("")
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = stringResource(Res.string.close_hint),
+                        tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+                    )
+                }
+            }
+        }
+    )
 }
